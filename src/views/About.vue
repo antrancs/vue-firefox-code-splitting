70f22a74448a67e81fd4b84504da39d8aefb3f25
An Thien Tran <tranthienan.90@gmail.com>
2019-01-22 22:25:38 +0100

    commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,3 +1,16 @@
 <template>
   <div class="about"><h1>This is an about page</h1></div>
 </template>
+
+<script lang="ts">
+import d3 from "d3";
+import lodash from "lodash";
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class About extends Vue {
+  created() {
+    console.log(lodash);
+  }
+}
+</script>
